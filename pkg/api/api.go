e049434462ebb21857750a6772174bcc9d4ffa8e
K1ender <146767945+K1ender@users.noreply.github.com>
2026-01-01 20:29:22 +0300

    feat(configloader): add config loading functionality
    * Implement Load function to read configuration from file or environment
    * Handle file existence checks and errors
    * Utilize cleanenv for environment variable loading

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,13 @@
 package api
 
-import "context"
+import (
+	"context"
+
+	"github.com/sharemymusic/shared/pkg/env"
+	"github.com/sharemymusic/shared/pkg/logger"
+)
 
 func Run(ctx context.Context) error {
+	ctx = logger.CreateContext(ctx, env.Development)
 	return nil
 }
